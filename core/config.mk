fcc4bffe2c1b6cc26acab48a2ae34b34247ea416
EunoiaOS <235215905+eunoia-committer@users.noreply.github.com>
2025-12-02 11:30:37 +0700

    EunoiaOS
    Change-Id: I8ac901eaa3d07a2c568b6c8d0df5a56eea0f9a05

--- core/config.mk
+++ core/config.mk
@@ -443,8 +443,8 @@ endif
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
 -include vendor/extra/BoardConfigExtra.mk
-ifneq ($(LINEAGE_BUILD),)
-include vendor/lineage/config/BoardConfigLineage.mk
+ifneq ($(EUNOIA_BUILD),)
+include vendor/os/config/BoardConfigEunoia.mk
 endif
 
 # The build system exposes several variables for where to find the kernel
@@ -1257,11 +1257,11 @@ dont_bother_goals := out product-graph
 # consistency with those defined in BoardConfig.mk files.
 include $(BUILD_SYSTEM)/android_soong_config_vars.mk
 
-ifneq ($(LINEAGE_BUILD),)
-ifneq ($(wildcard device/lineage/sepolicy/common/sepolicy.mk),)
+ifneq ($(EUNOIA_BUILD),)
+ifneq ($(wildcard device/eunoia/sepolicy/common/sepolicy.mk),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include device/lineage/sepolicy/common/sepolicy.mk)
+$(eval include device/eunoia/sepolicy/common/sepolicy.mk)
 endif
 endif
 
--- target/product/handheld_product.mk
+++ target/product/handheld_product.mk
@@ -33,7 +33,7 @@ PRODUCT_PACKAGES += \
     SettingsIntelligence \
     frameworks-base-overlays
 
-ifeq ($(LINEAGE_BUILD),)
+ifeq ($(EUNOIA_BUILD),)
 PRODUCT_PACKAGES += \
     Camera2 \
     LatinIME
